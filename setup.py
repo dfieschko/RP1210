48a3b5dd2e66611a5022de8fc8449946c028cece
Darius Fieschko <dfieschko@gmail.com>
2021-11-02 18:40:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'RP1210',
   packages = ['RP1210'],
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'A Python32 implementation of the RP1210C standard.',
   long_description = open('README.md').read(),
@@ -11,7 +11,6 @@ setup(
   author = 'Darius Fieschko',
   author_email = 'dfieschko@gmail.com',
   url = 'https://github.com/dfieschko/RP1210',
-  download_url = 'https://github.com/dfieschko/RP1210/archive/refs/tags/v0.0.1-alpha.tar.gz',
   keywords = ['RP1210', 'RP1210C', 'J1939'],
   install_requires=[],
   classifiers=[
