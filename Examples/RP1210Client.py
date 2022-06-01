73109b4fc2cfb8251acfba614f6905a1561eb11f
lisi12345 <renbihhs@gmail.com>
2022-06-01 11:07:35 -0500

    Update RP1210Client.py

--- Examples/RP1210Client.py
+++ Examples/RP1210Client.py
@@ -3,10 +3,9 @@ import RP1210
 
 # initialize RP1210Client and set vendor to DGDPA5MA
 client = RP1210.RP1210Client()
-client.setVendor("DGDPA5MA")
+client.setVendor("NULN2R32")
 
 # connect to adapter
-client.resetDevice()  # ? reallow connection?
 clientID = client.connect()
 print("Client ID: ", clientID, "\n\tTranslate:",
       RP1210.translateErrorCode(clientID))
@@ -38,10 +37,10 @@ print("All filters pass. Return code:", client.setAllFiltersToPass(),
 print("Set message fileting for J1939. Return code:", client.setJ1939Filters(
     0), "\n\tTranslate:", RP1210.translateErrorCode(client.setJ1939Filters(0)))
 
-# set message filtering for EXTENDED_CAN
-can_msg_filter = client.setCANFilters(0x01, 0xFFFFFFFF, 0xFFFFFFFF)  # ?
-print("Set message filtering for EXTENDED_CAN. Return code:",
-      can_msg_filter, "\n\tTranslate:", RP1210.translateErrorCode(can_msg_filter))
+# # set message filtering for EXTENDED_CAN
+# can_msg_filter = client.setCANFilters(0x01, 0xFFFFFFFF, 0xFFFFFFFF)  # ?
+# print("Set message filtering for EXTENDED_CAN. Return code:",
+#       can_msg_filter, "\n\tTranslate:", RP1210.translateErrorCode(can_msg_filter))
 
 # set echo transmitted messages
 print("Set echo transmitted messages. Return code:", client.setEcho(),
@@ -55,15 +54,15 @@ print("Set all filter states to discard. Return code:",
 print("Set message receive to True. Return code:", client.setMessageReceive(),
       "\n\tTranslate:", RP1210.translateErrorCode(client.setMessageReceive()))
 
-# protect J1939 address
-reserve_address = client.protectJ1939Address(0xF9, 0)  # ?
-print("Protect J1939 address. Return code:", reserve_address,
-      "\n\tTranslate:", RP1210.translateErrorCode(reserve_address))
+# # protect J1939 address
+# reserve_address = client.protectJ1939Address(0xF9, 0)  # ?
+# print("Protect J1939 address. Return code:", reserve_address,
+#       "\n\tTranslate:", RP1210.translateErrorCode(reserve_address))
 
-# release a J1939 address
-release_address = client.releaseJ1939Address(0xF9)  # ?
-print("Release a J1939 address. Return code:", release_address,
-      "\n\tTranslate:", RP1210.translateErrorCode(release_address))
+# # release a J1939 address
+# release_address = client.releaseJ1939Address(0xF9)
+# print("Release a J1939 address. Return code:", release_address,
+#       "\n\tTranslate:", RP1210.translateErrorCode(release_address))
 
 # set J1939 filter type to FILTER_INCLUSIVE
 J1939_filter_type = client.setJ1939FilterType(0)
@@ -76,17 +75,17 @@ print("Set CAN filter type to FILTER_INCLUSIVE. Return code:",
       can_filter_type, "\n\tTranslate:", RP1210.translateErrorCode(can_filter_type))
 
 # set J1939 broadcast interpacket timing
-J1939_broadcast_timing = client.setJ1939InterpacketTime(0x11111111)  # ?
+J1939_broadcast_timing = client.setJ1939InterpacketTime(10)
 print("Set J1939 broadcast interpacket timeing to 10ms. Return code:",
       J1939_broadcast_timing, "\n\tTranslate:", RP1210.translateErrorCode(J1939_broadcast_timing))
 
 # set max error message size to 250
-max_err_msg_size = client.setMaxErrorMsgSize(250)  # ?
+max_err_msg_size = client.setMaxErrorMsgSize(250)
 print("Set max error message size to 250. Return code:", max_err_msg_size,
       "\n\tTranslate:", RP1210.translateErrorCode(max_err_msg_size))
 
 # # disallow further client connections
-# disallow_conn = client.disallowConnections()  # ? re-allow?
+# disallow_conn = client.disallowConnections()  # ? can't find a way to re-allow further
 # print("Disallow further client connections. Return code:", disallow_conn,
 #       "\n\tTranslate:", RP1210.translateErrorCode(disallow_conn))
 # client1 = client.connect()
