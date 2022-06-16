7891cd2ca0dd06661547f234bacbb60eeb972c32
lisi12345 <renbihhs@gmail.com>
2022-06-16 12:44:01 -0500

    Create WriteDataByIdentifier.py

---
+++ RP1210/UDS/WriteDataByIdentifier.py
@@ -0,0 +1,41 @@
+from . import UDSMessage
+
+class WriteDataByIdentifierReqest(UDSMessage):
+    """
+    Write Data By Identifier (Request)
+    - `sid` = 0x2E
+    - `did` = dataIdentifier
+    - `data` = dataRecord (n bytes)
+    """
+
+    _sid = 0x2E
+    _isResponse = False
+
+    def __init__(self, did : int = 0x00, data : bytes = b''):
+        super().__init__()
+        self._hasSubfn = False
+        self._hasDID = True
+        self._hasData = True
+        self._dataSize = len(data)
+        self._dataSizeCanChange = True
+
+        self.did = did
+        self.data = data
+
+class WriteDataByIdentifierResponse(UDSMessage):
+    """
+    Write Data By Identifier (Response)
+    - `sid` = 0x6E
+    - `did` = dataIdentifier
+    """
+
+    _sid = 0x6E
+    _isResponse = True
+
+    def __init__(self, did : int = 0x00):
+        super().__init__()
+        self._hasSubfn = False
+        self._hasDID = True
+        self._hasData = False
+
+        self.did = did
