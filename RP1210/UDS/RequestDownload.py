74888b7b300fe065b28d0e6bfd4b64f7dbaee5eb
kali-muon <20001044+kali-muon@users.noreply.github.com>
2022-08-18 12:14:58 -0500

    Added comments to ReqDLReq

--- RP1210/UDS/RequestDownload.py
+++ RP1210/UDS/RequestDownload.py
@@ -10,9 +10,9 @@ class RequestDownloadRequest(UDSMessage):
     - `sid` = 0x34
     - `data` = dfid + alfid + maddr + msize
         - `dfid' = dataFormatIdentifier (1 byte)
-        - `alfid' = addressAndLengthFormateIdentifier (1 byte)
-            - bit 7 - 4: define length (number of bytes) of memoryAddress
-            - bit 3 - 0`: define length (number of bytes) of memorySize
+        - `alfid' = addressAndLengthFormatIdentifier (1 byte)
+            - bits 7-4: define length (number of bytes) of memorySize
+            - bits 3-0`: define length (number of bytes) of memoryAddress
         - `maddr` = memoryAddress (n bytes)
         - `msize` = memorySize (n bytes)
     """
@@ -21,65 +21,67 @@ class RequestDownloadRequest(UDSMessage):
     _isResponse = False
 
     def __init__(self, dfid: bytes = b'\x00', alfid: bytes = b'\x00', maddr: bytes = b'', msize: bytes = b'', autoALFID: bool = True):
+        """
+        - `dfid`: Data Format Identifier (1 byte). Default is `0x00` (no encryption or compression), other values are manufacturer-specific.
+        - `alfid`: Address and Length Format Identifer (1 byte). Can be left default if `autoALFID` is `True`.
+            - bits 7-4: define length (number of bytes) of msize
+            - bits 3-0: define length (number of bytes) of maddr
+        - `maddr`: memoryAddress (0-15 bytes)
+        - `msize`: memorySize (0-15 bytes)
+        - `autoALFID`: If `True`, `alfid` is automatically calculated based on the values of `maddr` and `msize`. If `False`, `alfid` must
+        be specified manually to produce a valid request.
+        """
         super().__init__()
-        self._hasSubfn = False
-        self._hasDID = False
-        self._hasData = True
-        self._dataSizeCanChange = True
+        self._hasSubfn:          bool = False
+        self._hasDID:            bool = False
+        self._hasData:           bool = True
+        self._dataSizeCanChange: bool = True
+
         self._autoALFID: bool = autoALFID
+        self.dfid             = dfid
+        self.alfid            = alfid
+        self.maddr            = maddr
+        self.msize            = msize
 
-        self.dfid = dfid
-        self.alfid = alfid
-        self.maddr = maddr
-        self.msize = msize
-        self._dataSize = len(self.data)
-
-        # self.did = int.from_bytes(sanitize_msg_param(dfid + alfid, 2), 'big') # remove later
-        # self.data = dfid + alfid + maddr + msize
+        self._dataSize: int = len(self.data)
 
     @property
     def dfid(self) -> int:
         return int.from_bytes(self._dfid, 'big')
 
     @dfid.setter
-    def dfid(self, dfid: int | bytes):
-        dfid = sanitize_msg_param(dfid, 1)
-        # if(dfid.__len__() != 1):
-        #     raise ValueError(F"DFID length must be 1 byte, got {dfid.__len__()} bytes: {dfid}")
-        self._dfid = dfid
+    def dfid(self, dfid: int | bytes) -> None:
+        self._dfid = sanitize_msg_param(dfid, 1)
     
     @property
     def alfid(self) -> int:
         return int.from_bytes(self._alfid, 'big')
 
     @alfid.setter
-    def alfid(self, alfid: int | bytes):
-        alfid = sanitize_msg_param(alfid, 1)
-        # if(alfid.__len__() != 1):
-        #     raise ValueError(F"ALFID length must be 1 byte, got {alfid.__len__()} bytes: {alfid}")
-        self._alfid = alfid
+    def alfid(self, alfid: int | bytes) -> None:
+        self._alfid = sanitize_msg_param(alfid, 1)
 
     @property
     def maddr(self) -> int:
         return int.from_bytes(self._maddr, 'big')
 
     @maddr.setter
-    def maddr(self, maddr: int | bytes):
+    def maddr(self, maddr: int | bytes) -> None:
         maddr = sanitize_msg_param(maddr)
-        if(self._autoALFID == True):
+        if(self._autoALFID == True): # Auto-calculating alfid
             if(maddr.__len__() > 0x0f):
                 raise ValueError(F"maddr length must be less than 16 bytes, got {maddr.__len__()} bytes: {maddr}")
             self.alfid = (self.alfid & 0xf0) | (maddr.__len__() & 0x0f)
         self._maddr = maddr
-    
+
     @property
     def msize(self) -> int:
         return int.from_bytes(self._msize, 'big')
 
     @msize.setter
-    def msize(self, msize: int | bytes):
+    def msize(self, msize: int | bytes) -> None:
         msize = sanitize_msg_param(msize)
-        if(self._autoALFID == True):
+        if(self._autoALFID == True): # Auto-calculating alfid
             if(msize.__len__() > 0x0f):
                 raise ValueError(F"msize length must be less than 16 bytes, got {msize.__len__()} bytes: {msize}")
             self.alfid = (self.alfid & 0x0f) | ((msize.__len__() & 0x0f) << 4)
@@ -87,11 +89,10 @@ class RequestDownloadRequest(UDSMessage):
     
     @property
     def data(self) -> bytes:
+        """
+        Concatenates the request's properties to form the data field.
+        """
         return sanitize_msg_param(self._dfid) + sanitize_msg_param(self._alfid) + sanitize_msg_param(self._maddr) + sanitize_msg_param(self._msize)
-
-    # @property
-    # def _dataSize(self) -> int:
-    #     return self.data.__len__()
 
 
 class RequestDownloadResponse(UDSMessage):
